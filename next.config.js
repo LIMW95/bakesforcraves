0f13a38b25bffe4761f7185c8127694faa413583
walim <walim@dswd.gov.ph>
2023-09-16 20:41:52 +0800

    try 2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-  
-   // Add basePath
-    basePath: '/github-pages',
-  
+  output: 'export',
+  images:{
+    unoptimized: true,
+  }
   }
   
   module.exports = nextConfig
