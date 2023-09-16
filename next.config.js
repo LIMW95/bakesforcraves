c1442fa548913e48b119aeb16a7c88839fd03b79
walim <walim@dswd.gov.ph>
2023-09-16 17:38:38 +0800

    declaration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+    target: 'serverless', // Ensure you have this line
+    // Other Next.js configuration options
+  };
+  
