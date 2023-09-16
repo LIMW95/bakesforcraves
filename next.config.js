b955f00527a4826cc6120ca8b8b55fa911e7f61d
walim <walim@dswd.gov.ph>
2023-09-16 21:50:48 +0800

    assetPrefix applied

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath:'/bakesforcraves',
+  assetPrefix: '/bakesforcraves/', // Add this line
   output: 'export',
   images:{
     unoptimized: true,
