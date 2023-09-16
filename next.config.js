363fa4959a2446f88de5f8be0911edf2afdde73d
walim <walim@dswd.gov.ph>
2023-09-16 19:27:46 +0800

    undo changes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-// next.config.js
-module.exports = {
-    exportPathMap: async function (defaultPathMap) {
-      return {
-        '/': { page: '/' },
-        // Add other routes as needed
-      };
-    },
-    // Other Next.js configuration options
-  };
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
